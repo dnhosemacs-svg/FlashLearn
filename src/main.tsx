dfc4dd9519870a5b9baaa1dc96aaa834e4bd8545
dnhosemacs-svg <dnhosemacs@gmail.com>
2026-04-22 12:41:36 +0200

    Modal aplicando styles.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import '@carbon/styles/css/styles.css'
 import './index.css'
 import App from './App.tsx'
 import CollectionsPage from './pages/CollectionsPage.tsx'
